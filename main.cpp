9dc9ae94ddac8da8dde39813ae2f0f5e74098494
origami233@hotmail.com <origami233@hotmail.com>
2024-02-20 18:19:29 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#pragma once
+#include "NeuralNetwork/NeuralNetwork.h"
+
+using namespace std;
 
 int main(int, char**){
-    std::cout << "Hello, from NeuralNetwork!\n" << std::endl;
+
+	ChaosLearningModel::NeuralNetwork NN;
+	
+	NN.Initialize({ 10,30,10 });
+
+	NN.vv_a[0][0] = 1;
+	NN.vv_a[0][1] = 2;
+
+	double a = NN.Summa(1, 0);
+
+	cout << a << endl << endl;
+
+	NN.Print();
+
+
 }
