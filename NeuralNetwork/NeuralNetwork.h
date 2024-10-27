6c4f3a088a071c1728a0c437c760105ebf7c4651
OrigamiGamer <3084943362@qq.com>
2024-10-27 22:51:32 +0800

    new model

--- NeuralNetwork/NeuralNetwork.h
+++ NeuralNetwork/NeuralNetwork.h
@@ -5,7 +5,6 @@
 namespace ChaosLearningModel {
 
 	// Computation
-
 	template <typename T>
 	std::vector<T> operator*(std::vector<T>& v_a, std::vector<T>& v_b) {
 		std::vector<T> v_c(v_a.size());
@@ -32,6 +31,8 @@ namespace ChaosLearningModel {
 		// [layer][node][connection]
 		VV_A vv_a;
 		VVV_A vvv_w;
+		// w[m][n]: the node[n] connected to the last layer[m]
+		// w[m][n][q]: weight of the connection[q] from the node[n] to the last layer[m]
 
 		void Initialize(VV_A initial_vv_a, VVV_A initial_vvv_w) {
 			vv_a = initial_vv_a;
@@ -40,11 +41,15 @@ namespace ChaosLearningModel {
 		void Initialize(size_t layer_count, std::vector<size_t> v_node_count_of_each_layer) {
 			vv_a.resize(layer_count);
 			vvv_w.resize(layer_count);
-			for (size_t layer = 0; layer < layer_count; layer++) {
-				vv_a[layer].resize(v_node_count_of_each_layer[layer]);
-				vvv_w[layer].resize(v_node_count_of_each_layer[layer]);
-				for (size_t node = 0; node < v_node_count_of_each_layer[layer]; node++) {
+			for (size_t m = 0; m < layer_count; m++) {
+				vv_a[m].resize(v_node_count_of_each_layer[m]);
 
+				// the final layer doesn't have connections to next layer
+				if (m < layer_count - 1) {
+					vvv_w[m].resize(v_node_count_of_each_layer[m]);
+					for (size_t n = 0; n < v_node_count_of_each_layer[m + 1]; n++) {
+						vvv_w[m][n].resize(v_node_count_of_each_layer[m]);
+					}
 				}
 			}
 		}
@@ -64,8 +69,6 @@ namespace ChaosLearningModel {
 	void debug() {
 		NeuralNetwork NN;
 		NN.Initialize(3, { 2,10,4 });
-
-
 
 	}
 
--- main.cpp
+++ main.cpp
@@ -5,21 +5,36 @@
 
 using namespace std;
 
+typedef std::vector<double> V_NODE;
+typedef std::vector<V_NODE> VV_NODE;
+typedef std::vector<VV_NODE> VVV_NODE;
+
+template <typename T>
+void operator+=(std::vector<T>& v1, std::vector<T> const& v2) {
+	for (size_t i = 0; i < v1.size();i++) {
+		v1.at(i) += v2.at(i);
+	}
+};
+
+template <typename T>
+void operator+=(std::vector<T>& v1, T const& a) {
+	for (size_t i = 0; i < v1.size();i++) {
+		v1.at(i) += a;
+	}
+};
+
+template <typename T>
+std::vector<T> operator*(VVV_NODE const& matrix_W, V_NODE const& v_a) {
+	V_NODE v_result;
+	for (size_t m = 0;m < matrix_W.size();m++) {
+		v_result += matrix_W.at(m) * v_a.at(m);
+	}
+	return v_result;
+};
+
+
 int main(int, char**) {
-	using namespace ChaosLearningModel;
-	debug();
+	// using namespace ChaosLearningModel;
 
-	// cout << string("./") << endl;
 
-	//// TEST
-	// VV_A a = { {2,2},{2,2},{2,2} };
-	// VV_A b = { {2,3},{4,5},{6,7} };
-
-	// VV_A c = a * b;
-
-	// for (auto ww : c) {
-	// 	for (auto w : ww)
-	// 		cout << w << " ";
-	// 	cout << endl;
-	// }
 }
